9a83dfae45f8ed5665a1b2f81ec4b88c77d92ad6
nraytselis <98825564+nraytselis@users.noreply.github.com>
2026-08-06 11:05:54 -0400

    Add files via upload

---
+++ Great_Lakes_YCS_analysis_8-5-26.R
@@ -0,0 +1,153 @@
+library(dplyr)
+library(tidyverse)
+
+setwd("~/Desktop/Whitefish/GreatLakes")
+
+recruitmentData = read_csv("GLOWFISH_SuperiorHuronMichiganErieOntarioSimcoe_Brown.csv")
+TP = read_csv("TP_GLNPO_Clean.csv") #total phosphorous
+WS = read_csv("Percent_Ice_Cover_Great_Lakes.csv") #winter severity - % ice cover data 
+
+TP$Season = as.factor(TP$Season)
+####Separate water bodies into new dfs
+
+###YCS - recruitment data for each of the Great Lakes + Lake Simcoe 
+Lake_Ontario = recruitmentData %>% filter(waterbody_name == "Ontario_Brown")
+
+Lake_Simcoe = recruitmentData %>% filter(waterbody_name == "Simcoe_Brown") #no TP or WS data currently 
+
+Lake_Erie = recruitmentData %>% filter(waterbody_name == "Erie_Brown")
+
+Lake_Huron = recruitmentData %>% filter(waterbody_name == "Huron_Brown")
+
+Lake_Michigan = recruitmentData %>% filter(waterbody_name == "Michigan_Brown")
+
+Lake_Superior = recruitmentData %>% filter(waterbody_name == "Superior_Brown")
+
+###Total Phosphorous (data are for Spring and Summer, taken across a few days, and cover multiple stations)
+TP_Ontario = TP %>% filter(Lake == "Ontario")
+  
+TP_Erie = TP %>% filter(Lake == "Erie")
+  
+TP_Huron = TP %>% filter(Lake == "Huron")
+
+TP_Michigan = TP %>% filter(Lake == "Michigan")
+
+TP_Superior = TP %>% filter(Lake == "Superior")
+
+
+###Winter Severity (% Ice cover)
+
+WS_Ontario = WS[, c(1,10,11)]
+
+WS_Erie = WS[, c(1,8,9)]
+
+WS_Huron = WS[, c(1,6,7)]
+  
+WS_Michigan = WS[, c(1,4,5)]
+  
+WS_Superior = WS[, c(1:3)]
+
+
+####YCS - over time####
+
+recruitmentData <- recruitmentData[!is.na(recruitmentData$data_name), ]
+
+ggplot(recruitmentData, aes(x = year_class, y = fish_abundance_value, group = data_name, color = data_name)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~ data_name) + theme_minimal() + labs(x = "Year Class", y = "YCS")
+
+
+####Look at how much variation in Total Phosphorous there is across sampling sites 
+#to see if we can take an average across all sites for a given lake/year
+
+####Lake Ontario####
+length(unique(TP_Ontario$Station)) #24 different stations 
+
+TP_Ontario_Summary = TP_Ontario %>% group_by(Year,Season) %>% summarise(AcrossLakeMeanTP = mean(Value), #mean across multiple stations
+                                                                 SD = sd(Value),
+                                                                 n = n(),
+                                                                 SE_TP = sd(Value)/sqrt(n))
+ggplot(TP_Ontario_Summary,
+       aes(x = Year,
+           y = AcrossLakeMeanTP, group = Season, color = Season)) +
+  geom_point() +
+  geom_errorbar(aes(
+    ymin = AcrossLakeMeanTP - SE_TP,
+    ymax = AcrossLakeMeanTP + SE_TP)) + theme_minimal() + labs(y = "Mean TP across all stations", title = "Lake Ontario")
+
+
+ggplot(WS_Ontario,aes(x = Year, y = Ontario_Cover)) + geom_point() + geom_line() + theme_minimal() +
+  labs(y = "% Ice Cover", title = "Lake Ontario")
+
+
+
+
+    
+####Key takeaways for Lake Ontario TP####
+      ####total phosphorous has decreased over time across all stations for both spring and summer 
+          #for now 
+
+####Combine dataframes to do analysis####
+
+Lake_Ontario = Lake_Ontario %>% mutate(Year = year_class) #copy over year class column into new year column 
+
+Lake_Ontario_EnvioData = merge(TP_Ontario_Summary,WS_Ontario,by = "Year")
+
+Lake_Ontario_alldata = merge(Lake_Ontario_EnvioData,Lake_Ontario, by = "Year")
+
+Lake_Ontario_alldata_select = Lake_Ontario_alldata %>% select(c(1,2,3,6,7,23,24,25))
+
+
+#GAM: YCS ~ Larval Growth + Mussels + TP + Winter Severity
+library(mgcv)
+
+
+Ontario_gam <- gam(fish_abundance_value ~ s(AcrossLakeMeanTP) + s(Ontario_Cover) + s(Season, bs = "re"), 
+             data = Lake_Ontario_alldata_select)
+
+summary(Ontario_gam)
+
+plot(Ontario_gam)
+
+####Key Takeaways - total phosphorous, but not winter severity, is a strong predictor of YCS in Lake Ontario####
+
+
+###Lake Huron
+TP_Huron_Summary = TP_Huron %>% group_by(Year,Season) %>% summarise(AcrossLakeMeanTP = mean(Value), #mean across multiple stations
+                                                                        SD = sd(Value),
+                                                                        n = n(),
+                                                                        SE_TP = sd(Value)/sqrt(n))
+#Unlabled rows are Winter sampling
+levels(TP_Huron_Summary$Season) <- c(levels(TP_Huron_Summary$Season), "Winter")
+TP_Huron_Summary$Season[is.na(TP_Huron_Summary$Season)] <- "Winter"
+
+ggplot(TP_Huron_Summary,
+       aes(x = Year,
+           y = AcrossLakeMeanTP, group = Season, color = Season)) +
+  geom_point() +
+  geom_errorbar(aes(
+    ymin = AcrossLakeMeanTP - SE_TP,
+    ymax = AcrossLakeMeanTP + SE_TP)) + theme_minimal() +
+  labs(y = "Mean TP cross all stations", title = "Lake Huron")
+
+
+ggplot(WS_Huron,aes(x = Year, y = Huron_Cover)) + geom_point() + geom_line() + theme_minimal() +
+  labs(y = "% Ice Cover", title = "Lake Huron")
+
+
+
+Lake_Huron = Lake_Huron %>% mutate(Year = year_class) #copy over year class column into new year column 
+
+Lake_Huron_EnvioData = merge(TP_Huron_Summary,WS_Huron,by = "Year")
+
+Lake_Huron_alldata = merge(Lake_Huron_EnvioData,Lake_Huron, by = "Year")
+
+Lake_Huron_alldata_select = Lake_Huron_alldata %>% select(c(1,2,3,6,7,23,24,25))
+
+Huron_gam <- gam(fish_abundance_value ~ s(AcrossLakeMeanTP) + s(Huron_Cover) + s(Season, bs = "re"), 
+                   data = Lake_Huron_alldata_select)
+
+summary(Huron_gam) #both ice cover and TP significant
+plot(Huron_gam)
+
